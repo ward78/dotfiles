df40f087137fdd493e7fd642fe54e43f4a511234
Corynthion <cw@ward78.com>
2021-08-17 16:49:55 -0400

    add gitignore

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	fmt.Println("Dotfiles")
+	return nil
+}
