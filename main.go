436df325d261cd3f5b0266d64a36fae7b8e3616a
Corynthion <cw@ward78.com>
2021-08-18 07:58:09 -0400

    added main.go

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"runtime"
+	"errors"
+	"os/user"
+//	"path/filepath"
 )
 
 func main() {
@@ -13,6 +18,37 @@ func main() {
 }
 
 func run() error {
-	fmt.Println("Dotfiles")
+	err := verifySystem()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func verifySystem() error {
+	if runtime.GOOS != "linux" {
+		return errors.New("script requires linux OS")
+	}
+	u, err := user.Current()
+	if err != nil {
+		return err
+	}
+	if u.Gid != "0" {
+		return errors.New("root/sudo access is required")
+	}
+	return nil
+}
+
+func backupRun() error {
+	oldLocation := "/var/www/html/test.txt"
+	newLocation := "/var/www/html/src/test.txt"
+	err := os.Rename(oldLocation, newLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
+
+func backupReload() error {
 	return nil
 }
